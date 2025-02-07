4652ea8ea9957069beb9d80f4057df0ec44213e6
Ashish <ashish_b@hs.iitr.ac.in>
2025-02-08 02:05:02 +0530

    input ui changed

--- Frontend/src/Components/Input.jsx
+++ Frontend/src/Components/Input.jsx
@@ -1,29 +1,15 @@
 import React, {useId} from 'react'
 
-const Input = React.forwardRef( function Input({
-    label,
-    type = "text",
-    className = "",
-    ...props
-}, ref){
-    const id = useId()
-    return (
-        <div className='w-full'>
-            {label && <label 
-            className='inline-block mb-1 pl-1' 
-            htmlFor={id}>
-                {label}
-            </label>
-            }
-            <input
-            type={type}
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            ref={ref}
-            {...props}
-            id={id}
-            />
-        </div>
-    )
-})
+const Input = ({ label, className, labelClassName, error, ...props }) => (
+    <div className="space-y-2">
+      {label && <label className={`block text-sm ${labelClassName}`}>{label}</label>}
+      <input
+        {...props}
+        className={`w-full transition-colors duration-200 ${className}`}
+      />
+      {error && <p className="text-red-400 text-xs mt-1">{error}</p>}
+    </div>
+  );
+  
 
 export default Input
